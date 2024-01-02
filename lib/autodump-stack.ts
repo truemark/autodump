0b4ff6f3398cea1be9f6d07fbeb6cc54a844dc79
Lisa Koivu <lisamarie.koivu@gmail.com>
2024-01-02 05:44:30 -0500

    compiles

--- lib/autodump-stack.ts
+++ lib/autodump-stack.ts
@@ -1,9 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {AutoDump} from "./autodump-construct";
+import { ExtendedStack, ExtendedStackProps } from "truemark-cdk-lib/aws-cdk";
 
-export class AutoDumpStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class AutoDumpStack extends ExtendedStack {
+  constructor(scope: Construct, id: string, props?: ExtendedStackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
@@ -12,5 +13,7 @@ export class AutoDumpStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'AutodumpQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // new AutoDump(this, "AutoDump", {})
   }
 }
---
+++ lib/enums.ts
@@ -0,0 +1,9 @@
+export enum AwsRegion {
+    Oregon = "us-west-2"
+}
+
+export enum AwsAccount {
+    Prod = "348901320172",
+    Dev = "062758075735",
+    Sandbox = "825434587220"
+}
