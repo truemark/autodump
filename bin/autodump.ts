7368dce65e72ebe9f4cee72205834d9e7a1cb6bd
Lisa Koivu <lisamarie.koivu@gmail.com>
2024-01-08 14:19:13 -0500

    code cleanup

--- bin/autodump.ts
+++ bin/autodump.ts
@@ -12,23 +12,5 @@ new AutoDumpStack(app, 'AutoDump', {
     env: { account: process.env.account, region: process.env.region},
 });
 
-// if (app.account === AwsAccount.LkSandbox && app.region === AwsRegion.Oregon) {
-//     console.log("deploying---------->" + app.account)
-// // Is it possible to reference a variable in the env statement?
-//     new AutoDumpStack(app, 'AutoDump', {
-//         env: { account: process.env.account, region: process.env.region},
-//     })
-// } else if  (app.account === AwsAccount.VoDev && app.region === AwsRegion.Oregon) {
-//     new AutoDumpStack(app, 'AutoDump', {
-//         env: { account: '062758075735', region: 'us-west-2' },
-//     })
-// } else if  (app.account === AwsAccount.SupplyChainDev && app.region === AwsRegion.Oregon) {
-//     new AutoDumpStack(app, 'AutoDump', {
-//         env: { account: process.env.account, region: process.env.region},
-//         // env: { account: '250585841971', region: 'us-west-2' },
-//     })
-// }
-// else {
-//     console.log("Nada" + app.account)
-// };
+
 
--- lib/autodump-stack.ts
+++ lib/autodump-stack.ts
@@ -14,28 +14,6 @@ export class AutoDumpStack extends ExtendedStack {
         this.addMetadata("Repository", p.repository.url);
         this.addMetadata("Homepage", p.homepage);
 
-//
-//
-//         if (app.account === AwsAccount.LkSandbox && app.region === AwsRegion.Oregon) {
-//             console.log("deploying---------->" + app.account)
-// // Is it possible to reference a variable in the env statement?
-//             new AutoDumpStack(app, 'AutoDump', {
-//                 env: { account: process.env.account, region: process.env.region},
-//             })
-//         } else if  (app.account === AwsAccount.VoDev && app.region === AwsRegion.Oregon) {
-//             new AutoDumpStack(app, 'AutoDump', {
-//                 env: { account: '062758075735', region: 'us-west-2' },
-//             })
-//         } else if  (app.account === AwsAccount.SupplyChainDev && app.region === AwsRegion.Oregon) {
-//             new AutoDumpStack(app, 'AutoDump', {
-//                 env: { account: process.env.account, region: process.env.region},
-//                 // env: { account: '250585841971', region: 'us-west-2' },
-//             })
-//         }
-//         else {
-//             console.log("Nada" + app.account)
-//         };
-
         new AutoDump(this, "AutoDump", {});
     }
 }
