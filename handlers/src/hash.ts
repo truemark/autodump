3520d985d2931a92b15b070f5061777ea09da2d1
Lisa Koivu <lisamarie.koivu@gmail.com>
2024-01-31 11:26:54 -0500

    hash function is working.

--- handlers/src/hash.ts
+++ handlers/src/hash.ts
@@ -1,5 +1,5 @@
 import {SecretsManagerClient, DescribeSecretCommand} from "@aws-sdk/client-secrets-manager";
-import {AutoDumpTags} from "./hash-helper";
+import {AutoDumpTags, hashTagsV1} from "./hash-helper";
 
 interface AutoDumpAction {
   readonly resourceId: string;
@@ -19,33 +19,34 @@ interface AutoDumpResource {
   readonly tagsHash: string;
 }
 
-// const currentRegion = process.env.AWS_REGION;
+const currentRegion = process.env.AWS_REGION;
 export async function handler(event: any): Promise<any> {
   const secretArn = event.secretArn;
   const hash = event.hash;
 
-  const currentRegion = process.env.AWS_REGION;
-
   const client = new SecretsManagerClient({region: currentRegion});
-  const input = { // DescribeSecretRequest
+  const input = {
     SecretId: secretArn
   };
   const command = new DescribeSecretCommand(input);
   const response = await client.send(command);
-  console.log(`response is ${JSON.stringify(response)}`)
+  console.log(`response is ${JSON.stringify(response.Tags)}`)
+  const currentHash = hashTagsV1(response.Tags);
+  console.log(`current hash is ${currentHash}`)
 
-  // Do logic
-  // get secret
-  // get tags on secret
-  // calculate tags on secret
-  // compare hashes and decide to continue or not
-
-  // This is your raw output for your step in your step function
-  return {
-    hash,
-    secretArn,
-    execute: false, // This is what you use in your choice to bail out
-    reason: 'Some reason', // This is your reason for bailing that helps you when debugging
-    // whatever else you need to pass to the next step
+  if (currentHash === hash) {
+    return {
+      hash,
+      secretArn,
+      execute: true,
+      reason: `Initial and current hashes match: ${hash} . Continue execution.`,
+    }
+  } else {
+    return {
+      hash,
+      secretArn,
+      execute: false,
+      reason: `Hashes do not match. Initial Hash: ${hash} , Current Hash: ${currentHash} . This means the schedule tag has changed since this task was initially scheduled.`
+    }
   }
 }
